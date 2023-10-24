aa7aa1ccc8b661a92ea27c47c3f56528aa6bb4bc
Malakelis <awgeorgis@gmail.com>
2023-10-24 10:57:58 -0700

    added tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,6 +9,19 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  /* @Test
+  public void testReverse() {
+    int[] input1 = {3, 5, 8};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] {8, 5, 3}, input1);
+  }
+*/
+
+@Test
+public void testAverageNoLowest() {
+  double[] input1 = {3, 5, 8};
+ assertEquals(6.5, ArrayExamples.averageWithoutLowest(input1), 0.001);
+}
 
   @Test
   public void testReversed() {
