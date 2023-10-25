57487191219ae1ae13386bf88ab39004a496a422
Tom Zhang <treez.zhang@gmail.com>
2023-10-25 15:10:32 -0700

    fix filter bug

--- ListTests.java
+++ ListTests.java
@@ -10,13 +10,18 @@ public class ListTests {
 
     @Test
     public void filterTest() {
-        StringChecker sc = s -> { return Objects.equals(s, "hello"); };
+        StringChecker sc = s -> { return Objects.equals(s, "hello") || Objects.equals(s, "hi"); };
 
         ArrayList<String> testList = new ArrayList<>();
+        testList.add("what");
         testList.add("hello");
+        testList.add("oh");
         testList.add("hi");
+        testList.add("how");
+        testList.add("are");
+        testList.add("you");
 
         List<String> filteredList = ListExamples.filter(testList, sc);
-        assertArrayEquals(new String[]{"hello"}, filteredList.toArray());
+        assertArrayEquals(new String[]{"hello",  "hi"}, filteredList.toArray());
     }
 }
