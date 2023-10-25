1850d51b3005568ac5588a598abacb657dd285f7
Tom Zhang <treez.zhang@gmail.com>
2023-10-25 14:27:17 -0700

    fmt

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,19 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+    @Test
+    public void testReverseInPlace() {
+        int[] input1 = {3};
+        ArrayExamples.reverseInPlace(input1);
+        assertArrayEquals(new int[]{3}, input1);
+    }
 
 
-  @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
+    @Test
+    public void testReversed() {
+        int[] input1 = {};
+        assertArrayEquals(new int[]{}, ArrayExamples.reversed(input1));
+    }
 }
--- FileExample.java
+++ FileExample.java
@@ -38,18 +38,18 @@ public class FileExample {
 
   */
 
-	static List<File> getFiles(File start) throws IOException {
-	  File f = start;
-	  List<File> result = new ArrayList<>();
-	  result.add(start);
-	  if(f.isDirectory()) {
-	    File[] paths = f.listFiles();
-	    for(File subFile: paths) {
-	      result.add(subFile);
-	    }
-	  }
-	  return result;
-	}
+    static List<File> getFiles(File start) throws IOException {
+        File f = start;
+        List<File> result = new ArrayList<>();
+        result.add(start);
+        if (f.isDirectory()) {
+            File[] paths = f.listFiles();
+            for (File subFile : paths) {
+                result.add(subFile);
+            }
+        }
+        return result;
+    }
 }
 
 
